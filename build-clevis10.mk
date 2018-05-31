031a73e0d0ad4599ab143ba49ba34589755306b3
blaufish <blaufish@users.noreply.github.com>
2018-05-31 14:28:19 +0200

    new improved makefile

--- build-clevis10.mk
+++ build-clevis10.mk
@@ -12,13 +12,18 @@ scp_to_build_server: check-env
 
 scp_back_to_git: check-env
 	scp -r \
-		$(SSH_REMOTE):debian-build-dir-clevis10/'*clevis-10*' \
-		$(SSH_REMOTE):debian-build-dir-clevis10/'*clevis_10*' \
+		$(SSH_REMOTE):debian-build-dir-clevis10/'*clevis?10*' \
+		$(SSH_REMOTE):debian-build-dir-clevis10/build-clevis10.mk \
 		.
 
 build:
-	-rm clevis-10/configure.ac clevis-10/src/Makefile.am clevis-10/src/clevis\
- clevis-10/src/clevis-decrypt-http clevis-10/src/systemd/clevis-luks-askpass clevis-10/tests/pin-http
-	tar xvf clevis_10.orig.tar.bz2 clevis-10
-	cd clevis-10 && dpkg-source --commit
+	-rm clevis-10/configure.ac clevis-10/src/ clevis-10/tests/
+	-cd debian && rm \
+ aclocal.m4      config.log      configure.ac    doc/            Makefile.in     test-driver\
+ autom4te.cache/ config.status   COPYING         install-sh      missing         tests\
+ compile         config.sub      Makefile        .pc             config.guess    configure\
+ depcomp         Makefile.am     src
+	#tar xvf clevis_10.orig.tar.bz2 clevis-10
+	#cd clevis-10 && dpkg-source --commit
+	cd clevis-10 && quilt pop -a -f
 	cd clevis-10 && DEB_BUILD_OPTIONS="nocheck" dpkg-buildpackage -us -uc -d
